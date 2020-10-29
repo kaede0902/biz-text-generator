3fcd9b08d5715721c03d5f0354826c1220e488d5
kaede0902 <kaede0920js@gmail.com>
2020-10-30 03:54:43 +0900

    init commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>biz-text-gen</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>ビジネス文章ジェネレーター</h1>
+    </div>
+  )
+}
