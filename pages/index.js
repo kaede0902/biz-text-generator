ffcbc7b45a1c83c1630f63a39555dfaf7bf5b971
kaede0902 <kaede0920js@gmail.com>
2020-10-30 04:11:02 +0900

    made layout js and module css and add them to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
@@ -7,7 +8,9 @@ export default function Home() {
         <title>biz-text-gen</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>ビジネス文章ジェネレーター</h1>
+      <Layout>
+          <h1>ビジネス文章ジェネレーター</h1>
+      </Layout>
     </div>
   )
 }
