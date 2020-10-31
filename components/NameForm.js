0c85389be755d085b4708f686dcedc9805edbc72
kaede0902 <kaede0920js@gmail.com>
2020-10-31 20:26:43 +0900

    add free area

--- components/NameForm.js
+++ components/NameForm.js
@@ -6,6 +6,7 @@ export default function NameForm () {
     const [state, setState] = useState({ 
         hisName: "採用担当者", hisCompany: "Next株式会社",
         myName: "私の名前", myCompany: "神奈川大学",
+        freeArea: "好きな文章を入れてね"
     });
     const handleChange = e => {
         const { name, value } = e.target;
@@ -34,11 +35,49 @@ export default function NameForm () {
             />
         )
     }
+    const myCompanyInput = () => {
+        return (
+            <input
+                value={state.myCompany}
+                type="text"
+                onChange={handleChange}
+                name="myCompany"
+            />
+        )
+    }
+    const myNameInput = () => {
+        return (
+            <input
+                value={state.myName}
+                type="text"
+                onChange={handleChange}
+                name="myName"
+            />
+        )
+    }
+    const freeAreaInput = () => {
+        return (
+            <textarea
+                value={state.freeArea}
+                type="text"
+                onChange={handleChange}
+                name="freeArea"
+            />
+        )
+    }
     const output = () => {
+        const firstGreeding = `お世話になっております`
+        const finalGreeding = `以上、よろしくお願いいたします`
         return (
             <Box m={2}>
                 <Paper elevation={5}>
                     <h2>{state.hisCompany}, {state.hisName}, 様</h2>
+                    <h2>{state.myCompany}, {state.myName}, です</h2>
+                    <h2>{firstGreeding}</h2>
+                    
+                    <h2>{state.freeArea}</h2>
+
+                    <h2>{finalGreeding}</h2>
                 </Paper>
             </Box>
         )
@@ -48,6 +87,10 @@ export default function NameForm () {
       <div>
             {hisCompanyInput()}
             {hisNameInput()}
+            {myCompanyInput()}
+            {myNameInput()}
+            <br/>
+            {freeAreaInput()}
             {output()}
       </div>
   )
