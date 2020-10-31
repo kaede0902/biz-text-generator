ea962866460348f5bada8c4afee8bdf0a9a1d723
kaede0902 <kaede0920js@gmail.com>
2020-10-31 18:56:44 +0900

    add container layout

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,16 @@
 import styles from './layout.module.css'
 
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+
 export default function layout ({children}) {
-  return <div className={styles.container}>{children}</div>
+  return (
+      <div className={styles.container}>
+          <Container>
+              <Paper>
+                  {children}
+              </Paper>
+          </Container>
+      </div>
+  )
 }
